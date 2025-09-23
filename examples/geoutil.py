38d423b2101c305688f59ab655dafc4c461e75be
Pierre M <6030167+devleaks@users.noreply.github.com>
2025-09-23 08:28:44 +0200

    utils for more examples

---
+++ examples/geoutil.py
@@ -0,0 +1,267 @@
+"""Earth-based geometry helper function and classes
+
+    With possible GeoJSON output, to send to geojson.io
+"""
+
+import math
+from dataclasses import dataclass
+
+from typing import List, Tuple, Dict
+
+
+R = 6378000  # Radius of third rock from the sun, in metres
+
+
+def angle_to_360(alfa: float) -> float:
+    beta = alfa % 360
+    if beta < 0:
+        beta = beta + 360
+    return beta
+
+
+def haversine(lat1: float, lat2: float, lon1: float, lon2: float) -> float:  # in radians.
+    dlat, dlon = lat2 - lat1, lon2 - lon1
+    return math.pow(math.sin(dlat / 2), 2) + math.cos(lat1) * math.cos(lat2) * math.pow(math.sin(dlon / 2), 2)
+
+
+def distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:  # in degrees.
+    lat1, lat2 = math.radians(lat1), math.radians(lat2)
+    lon1, lon2 = math.radians(lon1), math.radians(lon2)
+    a = haversine(lat1, lat2, lon1, lon2)
+    return 2 * R * math.asin(math.sqrt(a))  # in m
+
+
+def bearing_deg(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    y = math.sin(lon2 - lon1) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(lon2 - lon1)
+    t = math.atan2(y, x)
+    brng = angle_to_360(math.degrees(t))  # in degrees
+    return brng
+
+
+def destination(lat: float, lon: float, bearing_deg: float, d: float) -> Tuple[float, float]:
+    # From lat, lon, move d meters heading bearing_deg
+    lat = math.radians(lat)
+    lon = math.radians(lon)
+    brng = math.radians(bearing_deg)
+    r = d / R
+
+    lat2 = math.asin(math.sin(lat) * math.cos(r) + math.cos(lat) * math.sin(r) * math.cos(brng))
+    lon2 = lon + math.atan2(
+        math.sin(brng) * math.sin(r) * math.cos(lat),
+        math.cos(r) - math.sin(lat) * math.sin(lat2),
+    )
+    return (math.degrees(lat2), math.degrees(lon2))
+
+
+def point_in_polygon(point: Tuple[float, float], polygon: List[Tuple[float, float]]) -> bool:
+    # this will do. We do very local geometry (5000m around current location)
+    # pt is [x,y], pol is [[x,y],...]; should be "closed", pol[0] == pol[-1].
+    pt = point
+    pol = polygon
+    inside = False
+    for i in range(len(pol)):
+        x0, y0 = pol[i]
+        x1, y1 = pol[(i + 1) % len(pol)]
+        if not min(y0, y1) < pt[1] <= max(y0, y1):
+            continue
+        if pt[0] < min(x0, x1):
+            continue
+        cur_x = x0 if x0 == x1 else x0 + (pt[1] - y0) * (x1 - x0) / (y1 - y0)
+        inside ^= pt[0] > cur_x
+    return inside
+
+
+# Line = {start, end}, start, end are point
+# Point = {lat, lon}
+@dataclass
+class Point:
+    lat: float
+    lon: float
+
+    def geoson(self) -> Dict:
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [self.lon, self.lat],
+                "type": "Point",
+            },
+        }
+
+
+@dataclass
+class Line:
+    start: Point
+    end: Point
+
+    def geoson(self) -> Dict:
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [[self.start.lon, self.start.lat], [self.end.lon, self.end.lat]],
+                "type": "LineString",
+            },
+        }
+
+
+def mkLine(lat1: float, lon1: float, lat2: float, lon2: float) -> Line:
+    return Line(start=Point(lat1, lon1), end=Point(lat2, lon2))
+
+
+@dataclass
+class Polygon:
+    points: List[Point]
+
+    def geoson(self) -> Dict:
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [[(l.lon, l.lat) for l in self.points]],
+                "type": "Polygon",
+            },
+        }
+
+
+def line_intersect(line1: Line, line2: Line) -> Point | None:
+    # Finds intersection of line1 and line2. Returns Point() of intersection or None.
+    # !! Source code copied from GeoJSON code where coordinates are (longitude, latitude).
+    x1 = line1.start.lon
+    y1 = line1.start.lat
+    x2 = line1.end.lon
+    y2 = line1.end.lat
+    x3 = line2.start.lon
+    y3 = line2.start.lat
+    x4 = line2.end.lon
+    y4 = line2.end.lat
+    denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
+    numeA = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)
+    numeB = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)
+
+    if denom == 0:
+        if numeA == 0 and numeB == 0:
+            return None
+        return None
+
+    uA = numeA / denom
+    uB = numeB / denom
+
+    if uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1:
+        x = x1 + uA * (x2 - x1)
+        y = y1 + uA * (y2 - y1)
+        # return [x, y]  # x is longitude, y is latitude.
+        return Point(lat=y, lon=x)
+    return None
+
+
+# nearest_point_to_lines(p=Point(lat, lon), lines=[mkLine(lat1, lon1, lat2, lon2)])
+# def nearest_point_to_lines(p: Point, lines: List[Line]) -> Tuple[Point | None, float]:
+#     # First the nearest point to a collection of lines.
+#     # Lines is an array if Line()
+#     # Returns the point and and distance to it.
+#     dist = math.inf
+#     for line in lines:
+#         d1 = distance(p.lat, p.lon, line.start.lat, line.start.lon)
+#         d2 = distance(p.lat, p.lon, line.end.lat, line.end.lon)
+#         dl = max(d1, d2) * 2
+#         brng = bearing_deg(line.start.lat, line.start.lon, line.end.lat, line.end.lon)
+#         brng += 90  # perpendicular
+#         lat1,lon1 = destination(p.lat, p.lon, brng, dl)
+#         brng -= 180  # perpendicular
+#         lat2,lon2 = destination(p.lat, p.lon, brng, dl)
+#         perpendicular = Line(Point(lat1,lon1), Point(lat2,lon2))
+#         loni, lati = line_intersect(perpendicular, line)
+#         if loni is not None and lati is not None:
+#             d = distance(p.lat, p.lon, lati, loni)
+#             if d < dist:
+#                 dist = d
+#             return (Point(lati, loni), dist)
+#     return (None, dist)
+
+
+def nearest_point_to_line(p: Point, line: Line) -> Tuple[Point | None, float]:
+    d1 = distance(p.lat, p.lon, line.start.lat, line.start.lon)
+    d2 = distance(p.lat, p.lon, line.end.lat, line.end.lon)
+    dl = max(d1, d2)
+    brng = bearing_deg(line.start.lat, line.start.lon, line.end.lat, line.end.lon)
+    brng += 90  # perpendicular
+    lat1, lon1 = destination(p.lat, p.lon, brng, dl)
+    brng -= 180  # perpendicular
+    lat2, lon2 = destination(p.lat, p.lon, brng, dl)
+    perpendicular = Line(Point(lat1, lon1), Point(lat2, lon2))
+    intersect = line_intersect(perpendicular, line)
+    return (intersect, distance(p.lat, p.lon, intersect.lat, intersect.lon)) if intersect is not None else (None, 0)
+
+
+class GeoJSONIO:
+    # Simple text/dict manipulation to present geographic geometries in GeoJSON for display on geojson.io.
+    # Later: Add coloring
+
+    def __init__(self):
+        self.collection = []
+
+    def add(self, feature: dict):
+        self.collection.append(feature)
+
+    def feature_collection(self) -> dict:
+        return {
+            "type": "FeatureCollection",
+            "features": self.collection,
+        }
+
+    @staticmethod
+    def point(lat: float, lon: float):
+        # "properties": {
+        #   "marker-color": "#e32400",
+        #   "marker-size": "medium",
+        #   "marker-symbol": "circle"
+        # },
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [lon, lat],
+                "type": "Point",
+            },
+        }
+
+    @staticmethod
+    def line(lat1: float, lon1: float, lat2: float, lon2: float):
+        # "properties": {
+        #   "stroke": "#ff40ff",
+        #   "stroke-width": 2,
+        #   "stroke-opacity": 1
+        # },
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [[lon1, lat1], [lon2, lat2]],
+                "type": "LineString",
+            },
+        }
+
+    @staticmethod
+    def polygon(points: List[Tuple[float, float]]):
+        # "properties": {
+        #   "stroke": "#fffb00",
+        #   "stroke-width": 2,
+        #   "stroke-opacity": 1,
+        #   "fill": "#0433ff",
+        #   "fill-opacity": 0.5
+        # },
+        return {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [[(p[1], p[0]) for p in points]],
+                "type": "Polygon",
+            },
+        }
---
+++ examples/unitutil.py
@@ -0,0 +1,289 @@
+"""Conversion utility functions.
+
+Why doesn't airline business use MKSA?
+There are only 3 countries in the world that are still officially using the imperial system: The United States of America, Myanmar, and Liberia.
+"""
+
+from typing import Tuple
+
+
+########################################
+# Units, etc
+#
+FT = 12 * 0.0254  # 1 foot = 12 inches = 0.3048m
+NAUTICAL_MILE = 1.852  # Nautical mile in meters 6076.118ft=1nm. Easy.
+
+
+def sign(x: float | int):
+    # why does this function not exists in python?
+    return -1 if x < 0 else (0 if abs(x) == 0 else 1)  # -0 is not 0.
+
+
+class convert:
+    @staticmethod
+    def dms_to_dd(degrees, minutes, seconds, direction) -> float:
+        dd = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
+        return dd if direction in ("N", "E") else dd * -1
+
+    @staticmethod
+    def cifp_alt_in_ft(s: str) -> int | None:
+        """Convert altitude as specified in ARINC424 to int.
+        Altitude can be 12345 or FL250, or -1234 if below sea level.
+        Always 5 characters, unused char should be space. Can be "  123" or "123  ".
+        """
+        s = s.strip()
+        if s == "":
+            return None
+        if s.startswith("FL"):
+            return int(s[2:]) * 100
+        return int(s)
+
+    @staticmethod
+    def cifp_alt_in_fl(alt: float) -> str:
+        if alt > 15000 and alt == (100 * int(alt / 100)):
+            return f"FL{int(alt/100)}"
+        return f"{alt}"
+
+    @staticmethod
+    def cifp_speed(s: str) -> int | None:
+        """Convert speed in ARINC424 to float."""
+        s = s.strip()
+        if s == "":
+            return None
+        return int(s)
+
+    @staticmethod
+    def m_to_nm(m: float) -> float:
+        """
+        Convert meter to nautical miles
+
+        :param      m:    { parameter_description }
+        :type       m:    { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        return m / NAUTICAL_MILE
+
+    @staticmethod
+    def meters_to_feet(m: float) -> float:
+        """
+        Convert meter to feet
+
+        :param      m:    { parameter_description }
+        :type       m:    { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        return m / FT
+
+    @staticmethod
+    def meters_to_fl(m: float, rounding: int = -1) -> int:
+        """
+        Convert meter to feet
+
+        :param      m:    { parameter_description }
+        :type       m:    { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        r = (m / FT) / 100
+        if rounding != -1:
+            r = rounding * round(r / rounding)
+        return int(r)
+
+    @staticmethod
+    def fl_to_meters(fl: float) -> float:
+        """
+        Convert meter to feet
+
+        :param      m:    { parameter_description }
+        :type       m:    { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        return fl * 100 * FT
+
+    @staticmethod
+    def feet_to_meters(ft: float) -> float:
+        """
+        Convert feet to meters
+
+        :param      f:    { parameter_description }
+        :type       f:    { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        return ft * FT
+
+    @staticmethod
+    def kmh_to_kn(kmh: float) -> float:
+        """
+        Convert kilometer per hours into knots
+
+        :param      kmh:  The kilometers per hour
+        :type       kmh:  { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        return kmh / NAUTICAL_MILE
+
+    @staticmethod
+    def ms_to_kn(ms: float, r: int | None = None) -> float:
+        """
+        Convert meters per second into knots
+
+        :param      ms:  The meters per second
+        :type       kmh:  { type_description }
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        s = convert.kmh_to_kn(kmh=convert.ms_to_kmh(ms))
+        if r is not None:
+            s = round(s, r)
+        return s
+
+    @staticmethod
+    def kmh_to_ms(kmh: float) -> float:
+        """
+        Convert kilometer per hours into meters/second
+
+        float kmh: speed in kilometers per hour
+
+        float:    speed in meters per second
+        """
+        return kmh / 3.6
+
+    @staticmethod
+    def ms_to_kmh(ms: float) -> float:
+        return ms * 3.6
+
+    @staticmethod
+    def kn_to_kmh(kn: float) -> float:
+        return kn * NAUTICAL_MILE
+
+    @staticmethod
+    def kn_to_ms(kn: float) -> float:
+        return convert.kmh_to_ms(kn * NAUTICAL_MILE)
+
+    @staticmethod
+    def ms_to_fpm(ms: float, r: int | None = None) -> float:
+        s = convert.meters_to_feet(ms * 60)
+        if r is not None:
+            s = round(s, r)
+        return s
+
+    @staticmethod
+    def fpm_to_ms(fpm: float) -> float:
+        return convert.feet_to_meters(fpm / 60)
+
+    @staticmethod
+    def km_to_nm(km: float) -> float:
+        return km / NAUTICAL_MILE
+
+    @staticmethod
+    def nm_to_km(nm: float) -> float:
+        return nm * NAUTICAL_MILE
+
+    @staticmethod
+    def nm_to_meters(nm: float) -> float:
+        return nm * NAUTICAL_MILE * 1000
+
+    @staticmethod
+    def fl_to_m(fl: int) -> float:
+        return fl * FT / 100
+
+    @staticmethod
+    def mach_to_speeds(mach: float, altitude: int = 30000) -> Tuple[float, float, float]:
+        mph_machconvert = mach * 660
+        kmh_machconvert = mach * 1062
+        knots_machconvert = mach * 573
+
+        if altitude >= 40000:
+            mph_machconvert = mach * 660
+            kmh_machconvert = mach * 1062
+            knots_machconvert = mach * 573
+
+        elif altitude >= 35000:
+            mph_machconvert = mach * 664
+            kmh_machconvert = mach * 1069
+            knots_machconvert = mach * 577
+
+        elif altitude >= 30000:
+            mph_machconvert = mach * 679
+            kmh_machconvert = mach * 1093
+            knots_machconvert = mach * 590
+
+        elif altitude >= 25000:
+            mph_machconvert = mach * 693
+            kmh_machconvert = mach * 1116
+            knots_machconvert = mach * 602
+
+        elif altitude >= 20000:
+            mph_machconvert = mach * 707
+            kmh_machconvert = mach * 1138
+            knots_machconvert = mach * 614
+
+        elif altitude >= 15000:
+            mph_machconvert = mach * 721
+            kmh_machconvert = mach * 1161
+            knots_machconvert = mach * 627
+
+        elif altitude >= 10000:
+            mph_machconvert = mach * 735
+            kmh_machconvert = mach * 1182
+            knots_machconvert = mach * 638
+
+        elif altitude >= 0:
+            mph_machconvert = mach * 762
+            kmh_machconvert = mach * 1223
+            knots_machconvert = mach * 660
+
+        else:
+            mph_machconvert = mach * 660
+            kmh_machconvert = mach * 1062
+            knots_machconvert = mach * 573
+
+        return (kmh_machconvert, knots_machconvert, mph_machconvert)
+
+    @staticmethod
+    def mach_to_kmh(mach: float, altitude: int = 30000) -> float:
+        """
+        Convert MACH speed to ground speed for different altitude ranges.
+        Altitude should be supplied in feet ASL.
+        Returns kilometers per hour.
+
+        :param      mach:      The mach
+        :type       mach:      float
+        :param      altitude:  The altitude
+        :type       altitude:  int
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        c = convert.mach_to_speeds(mach, altitude)
+        return c[0]
+
+    @staticmethod
+    def mach_to_ms(mach: float, altitude: int = 30000) -> float:
+        """
+        Convert MACH speed to ground speed for different altitude ranges.
+        Altitude should be supplied in feet ASL.
+        Returns kilometers per hour.
+
+        :param      mach:      The mach
+        :type       mach:      float
+        :param      altitude:  The altitude
+        :type       altitude:  int
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
+        """
+        c = convert.mach_to_speeds(mach, altitude)
+        return c[0] / 3.6
